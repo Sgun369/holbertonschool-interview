c913c4ca446b995b61a38af835cd43554a648d19
Sgun369 <souhaiebjebali@gmail.com>
2024-05-30 11:54:20 +0100

    0-minoperations.py

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhton3
+"""Minimum Operations"""
+
+
+def minOperations(n):
+    """Minimum Operations"""
+    if n < 2:
+        return 0
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    return operations
