30d25098bad56dbad3f47b087d58eaa5f8d9b36c
Sgun369 <souhaiebjebali@gmail.com>
2024-06-05 11:19:40 +0100

    sandpiles.h

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -3,13 +3,14 @@
 
 #define SIZE 3
 #define MAX_GRAINS 3
-void print_grid(int grid[SIZE][SIZE]);  /* Remove 'static' */
+#define N SIZE
+void print_grid(int grid[N][N]);  /* Remove 'static' */
 
-int is_stable(int grid[SIZE][SIZE]);
+int is_stable(int grid[N][N]);
 
-void topple(int grid[SIZE][SIZE]);
+void topple(int grid[N][N]);
 
-void sandpiles_sum(int grid1[SIZE][SIZE], int grid2[SIZE][SIZE]);
+void sandpiles_sum(int grid1[N][N], int grid2[N][N]);
 
 
 
