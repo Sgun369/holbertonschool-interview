8945d6a5704e38e2b543c9a311961221375e7c71
Sgun369 <souhaiebjebali@gmail.com>
2024-06-05 11:03:05 +0100

    fix betty and other things

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-# include "sandpiles.h"
+#include "sandpiles.h"
 
 /**
- * Function to print a 3*3 grid.
+ * print_grid - Function to print a 3x3 grid.
  *
- * @param grid The 3*3 grid to be printed
+ * @grid: The 3x3 grid to be printed
 */
-
-void print_grid(int(grid[SIZE][SIZE]))
+void print_grid(int grid[SIZE][SIZE])
 {
-	for (int i = 0; i < SIZE; i++)
+	int i, j;
+
+	for (i = 0; i < SIZE; i++)
 	{
-		for (int j = 0; j < SIZE; j++)
+		for (j = 0; j < SIZE; j++)
 		{
 			if (j != 0)
 			{
@@ -22,16 +23,20 @@ void print_grid(int(grid[SIZE][SIZE]))
 		printf("\n");
 	}
 }
+
 /**
- * Function to check if the sandpile frid is stable.
- * @param grid The 3*3 grid to be checked
- * @return 1 if the grid is stable , 0 otherwise
+ * is_stable - Function to check if the sandpile grid is stable.
+ *
+ * @grid: The 3x3 grid to be checked
+ * Return: 1 if the grid is stable, 0 otherwise
 */
 int is_stable(int grid[SIZE][SIZE])
 {
-	for (int i = 0; i < SIZE; i++)
+	int i, j;
+
+	for (i = 0; i < SIZE; i++)
 	{
-		for (int j = 0; j < SIZE; j++)
+		for (j = 0; j < SIZE; j++)
 		{
 			if (grid[i][j] > MAX_GRAINS)
 			{
@@ -41,52 +46,57 @@ int is_stable(int grid[SIZE][SIZE])
 	}
 	return (1);
 }
+
 /**
- * Function to topple the sandpile.
+ * topple - Function to topple the sandpile.
  *
- * @param grid The 3*3 grid to be toppled.
+ * @grid: The 3x3 grid to be toppled
 */
 void topple(int grid[SIZE][SIZE])
 {
+	int i, j;
 	int temp[SIZE][SIZE] = {0};
 
-	for (int i = 0; i < SIZE; i++)
+	for (i = 0; i < SIZE; i++)
 	{
-		for (int j = 0; j < SIZE; j++)
+		for (j = 0; j < SIZE; j++)
 		{
 			if (grid[i][j] > MAX_GRAINS)
 			{
 				temp[i][j] -= 4;
 				if (i > 0)
-				temp[i - 1][j] += 1;
+					temp[i - 1][j] += 1;
 				if (i < SIZE - 1)
-				temp[i + 1][j] += 1;
-                if (j > 0)
-				temp[i][j - 1] += 1;
-                if (j < SIZE - 1)
-				temp[i][j + 1] += 1;
+					temp[i + 1][j] += 1;
+				if (j > 0)
+					temp[i][j - 1] += 1;
+				if (j < SIZE - 1)
+					temp[i][j + 1] += 1;
 			}
 		}
 	}
-	for (int i = 0; i < SIZE; i++)
+	for (i = 0; i < SIZE; i++)
 	{
-		for (int j = 0; j < SIZE; j++)
+		for (j = 0; j < SIZE; j++)
 		{
 			grid[i][j] += temp[i][j];
 		}
 	}
 }
+
 /**
- * Function to compute the sum of two sandpiles and stabilize the result.
- * 
- * @param grid1 The first 3*3 grid
- * @param grid2 The second 3*3 grid
+ * sandpiles_sum - Function to compute the sum of two sandpiles
+ *
+ * @grid1: The first 3x3 grid
+ * @grid2: The second 3x3 grid
 */
 void sandpiles_sum(int grid1[SIZE][SIZE], int grid2[SIZE][SIZE])
 {
-	for (int i = 0; i < SIZE; i++)
+	int i, j;
+
+	for (i = 0; i < SIZE; i++)
 	{
-		for (int j = 0; j < SIZE; j++)
+		for (j = 0; j < SIZE; j++)
 		{
 			grid1[i][j] += grid2[i][j];
 		}
