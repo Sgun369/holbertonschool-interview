5511e6aba10a9282111e7e01e611c5fd05c901a3
Sgun369 <souhaiebjebali@gmail.com>
2024-12-23 15:45:34 +0100

    heap

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -3,71 +3,52 @@
 #include "sort.h"
 
 /**
- * swap - Swap two elements in an array
- * @array: The array
- * @i: Index of the first element
- * @j: Index of the second element
+ * sift_down - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size:size of the array
+ * @root: the root of the heap
+ * @end: the boundary of the heap
  */
-void swap(int *array, int i, int j)
+void sift_down(int *array, size_t size, size_t root, size_t end)
 {
-	int temp = array[i];
+	size_t higher = root;
+	size_t left = 2 * root + 1;
+	size_t right = 2 * root + 2;
 
-	array[i] = array[j];
-	array[j] = temp;
-}
-
-/**
- * sift_down - Move the element at index i down
- * @array: The array
- * @size: Size of the heap
- * @i: Index to start sifting down from
- */
-void sift_down(int *array, size_t size, size_t i)
-{
-	size_t largest = i;
-	size_t left = 2 * i + 1;
-	size_t right = 2 * i + 2;
-
-	if (left < size && array[left] > array[largest])
-		largest = left;
-
-	if (right < size && array[right] > array[largest])
-		largest = right;
-
-	if (largest != i)
+	if (left < end && array[left] > array[higher])
+		higher = left;
+	if (right < end && array[right] > array[higher])
+		higher = right;
+	if (higher != root)
 	{
-		swap(array, i, largest);
+		int tmp = array[root];
+
+		array[root] = array[higher];
+		array[higher] = tmp;
 		print_array(array, size);
-		sift_down(array, size, largest);
+		sift_down(array, size, higher, end);
 	}
 }
-
 /**
- * build_max_heap - Build a max-heap from the input array
- * @array: The array
- * @size: Size of the array
- */
-void build_max_heap(int *array, size_t size)
-{
-	for (size_t i = size / 2; i-- > 0;)
-		sift_down(array, size, i);
-}
-
-/**
- * heap_sort - Sort an array of integers in ascending order
- * @array: The array to be sorted
- * @size: Size of the array
+ * heap_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size:size of the array
  */
 void heap_sort(int *array, size_t size)
 {
-	build_max_heap(array, size);
+	int i;
 
-	for (size_t i = size - 1; i > 0; i--)
+	if (size < 2)
+		return;
+	for (i = size / 2 - 1; i >= 0; i--)
+		sift_down(array, size, i, size);
+	for (i = size - 1; i > 0; i--)
 	{
-		swap(array, 0, i);
+		int tmp = array[0];
+
+		array[0] = array[i];
+		array[i] = tmp;
 		print_array(array, size);
-		sift_down(array, i, 0);
+		sift_down(array, size, 0, i);
 	}
 }
-
-
