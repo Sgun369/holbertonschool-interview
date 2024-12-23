7bfd3c030c4fcbddb1661c3bbb59950265156b86
Sgun369 <souhaiebjebali@gmail.com>
2024-12-23 15:46:04 +0100

    h

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -3,7 +3,7 @@
 #include <stddef.h>
 
 void heap_sort(int *array, size_t size);
-void sift_down(int *array, size_t size, size_t i);
+void sift_down(int *array, size_t size, size_t root, size_t end);
 void build_max_heap(int *array, size_t size);
 void swap(int *array, int i, int j);
 void print_array(int *array, size_t size);
