1e9da5e216881f5e0d04c3ccba09e4b29c8a5882
Sgun369 <souhaiebjebali@gmail.com>
2025-03-03 11:54:23 +0100

    0

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -3,13 +3,14 @@
 Module to calculate the perimeter of an island.
 """
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in the grid.
-    
+
     """
     perimeter = 0
-    
+
     # Iterate through each cell in the grid
     for i in range(len(grid)):
         for j in range(len(grid[i])):
@@ -23,5 +24,5 @@ def island_perimeter(grid):
                     perimeter += 1
                 if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:  # Check right
                     perimeter += 1
-    
+
     return perimeter
