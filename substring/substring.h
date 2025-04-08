2bbe5d2c938a1a79fd75162a708d81df406e3687
Sgun369 <souhaiebjebali@gmail.com>
2025-04-08 10:28:31 +0100

    substring

--- substring/substring.h
+++ substring/substring.h
@@ -1,6 +1,6 @@
 #ifndef SUBSTRING_H
 #define SUBSTRING_H
 int *find_substring(char const *s, char const **words, int nb_words, int *n);
-int match_words(const char *start, int word_len, const char **words
-int nb_words, int *ref_count);
+int match_words(const char *start, int word_len, const char **words,
+	int nb_words, int *ref_count);
 #endif
